a6101b69ccae61e8e00aa88119bed2ee816c8bc2
Vicluar Daniel Merchan Briceno <vicluar@gmail.com>
2020-02-01 19:48:32 +0100

    Adding classes for required functionality.

---
+++ WebAPI_Versioning/WebAPI_Versioning/ApiModel/Character.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI_Versioning.ApiModel
+{
+    public class Character
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AlterEgo { get; set; }
+    }
+}
