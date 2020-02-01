64286d5979687bec9d48256fcb4176d857a78797
Vicluar Daniel Merchan Briceno <vicluar@gmail.com>
2020-02-01 21:23:58 +0100

    Adding characters of version 2.0.

--- WebAPI_Versioning/WebAPI_Versioning/Controllers/DarkKnightTrilogyController.cs
+++ WebAPI_Versioning/WebAPI_Versioning/Controllers/DarkKnightTrilogyController.cs
@@ -9,19 +9,37 @@ namespace WebAPI_Versioning.Controllers
 {
     [ApiController]
     [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
     [Route("api/v{version:apiVersion}/[controller]")]
     public class DarkKnightTrilogyController : ControllerBase
     {
+        [HttpGet]
         public IActionResult GetCharacters()
         {
             var result = new List<Character>
             {
                 new Character { FirstName = "Bruce", LastName = "Wayne", AlterEgo = "Batman", Actor = "Christian Bale" },
-                new Character { FirstName = "Rachel", LastName = "Daws", AlterEgo = "", Actor = "Katie Holmes" },
+                new Character { FirstName = "Rachel", LastName = "Dawes", AlterEgo = "", Actor = "Katie Holmes" },
                 new Character { FirstName = "Jonathan", LastName = "Crane", AlterEgo = "Scarecrow", Actor = "Cillian Murphy" },
                 new Character { FirstName = "James", LastName = "Gordon", AlterEgo = "", Actor = "Gary Oldman" },
                 new Character { FirstName = "Alfred", LastName = "Pennyworth", AlterEgo = "", Actor = "Michael Caine" },
                 new Character { FirstName = "Henri", LastName = "Ducard", AlterEgo = "Ras al Gul", Actor = "Liam Neeson" }
+            };
+
+            return new OkObjectResult(result);
+        }
+
+        [HttpGet, MapToApiVersion("2.0")]
+        public IActionResult GetCharactersV2()
+        {
+            var result = new List<Character>
+            {
+                new Character { FirstName = "Bruce", LastName = "Wayne", AlterEgo = "Batman", Actor = "Christian Bale" },
+                new Character { FirstName = "Rachel", LastName = "Dawes", AlterEgo = "", Actor = "Maggie Gyllenhaal" },
+                new Character { FirstName = "James", LastName = "Gordon", AlterEgo = "", Actor = "Gary Oldman" },
+                new Character { FirstName = "Alfred", LastName = "Pennyworth", AlterEgo = "", Actor = "Michael Caine" },
+                new Character { FirstName = "", LastName = "", AlterEgo = "The Joker", Actor = "Heath Ledger" },
+                new Character { FirstName = "Harvey", LastName = "Dent", AlterEgo = "Two Faces", Actor = "Aaron Eckhart" },
             };
 
             return new OkObjectResult(result);
